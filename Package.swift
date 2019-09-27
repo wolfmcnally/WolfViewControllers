2dcfcebb0424eedddc2e002e95154fa12950ca63
Wolf McNally <wolf@wolfmcnally.com>
2019-09-27 14:26:23 -0700

    Added support for Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "WolfViewControllers",
+    platforms: [
+        .iOS(.v12), .tvOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "WolfViewControllers",
+            targets: ["WolfViewControllers"]),
+        ],
+    dependencies: [
+        .package(url: "https://github.com/wolfmcnally/WolfCore", from: "5.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfLog", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfLocale", from: "2.0.0"),
+        .package(url: "https://github.com/wolfmcnally/WolfViews", from: "4.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "WolfViewControllers",
+            dependencies: [
+                "WolfCore",
+                "WolfLog",
+                "WolfLocale",
+                "WolfViews",
+            ])
+        ]
+)
